3d94b1bb77462e641e7cb58cdc3dfde17cdf77bf
Paarth Arora <paarth@Paarths-MacBook-Pro.local>
2020-09-26 12:23:41 +1000

    Small changes

--- setup.R
+++ setup.R
@@ -2,4 +2,12 @@
 
 # Load packages
 library(tidyverse)
-  
+
+# Read data
+team_data <- read_csv("data/epl2020.csv")
+player_data <- read_csv("data/players_1920_fin.csv")
+
+# Clean data
+team_data <- team_data %>%  
+  select(-X1) # removing the first col because it is not helpful 
+
